4b2deea1be1b9a5b088b820be5f0a3ace0cd212f
pelancas <isafilara@gmail.com>
2026-03-09 23:27:24 -0300

    refazendo calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import Consultoria from "./pages/Consultoria";
 import AnaliseCarteira from "./pages/AnaliseCarteira";
 import ConsultoriaFinanceira from "./pages/ConsultoriaFinanceira";
 import Ferramentas from "./pages/Ferramentas";
-import ComparadorRF from "./comparador-renda-fixa";
 import Fundos from "./pages/investimentos/Fundos";
 import NotFound from "./pages/NotFound";
 
@@ -33,7 +32,6 @@ const App = () => (
           <Route path="/consultoria/consultoriafinanceira" element={<ConsultoriaFinanceira />} />
           <Route path="/ferramentas" element={<Ferramentas />} />
           <Route path="/ferramentas/:tool" element={<Ferramentas />} />
-          <Route path="/ferramentas/comparadorrf" element={<ComparadorRF />} />
           <Route path="/investimentos/fundos" element={<Fundos />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
