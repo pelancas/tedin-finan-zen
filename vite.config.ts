f67f512472f4e0a8ee2d4b6dcad9e62487271721
Isadora Lara <seuemail@email.com>
2026-03-17 10:49:08 -0300

    novo icone

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/tedin-finan-zen/',
   server: {
     host: "::",
     port: 8080,
